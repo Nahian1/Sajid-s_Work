6b69ef1a7b633b4011caf66ec5e570f129c6fe0d
nabilnealsh <nabil.shawkat023@gmail.com>
2018-05-24 23:29:07 +0600

    24052018 2329

--- app/src/main/java/com/cryptenet/thanatos/dtmweb/home/initiator_project/InitiatorProjectFragment.java
+++ app/src/main/java/com/cryptenet/thanatos/dtmweb/home/initiator_project/InitiatorProjectFragment.java
@@ -13,6 +13,8 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.cryptenet.thanatos.dtmweb.R;
@@ -25,6 +27,10 @@ public class InitiatorProjectFragment extends BaseFragment<InitiatorProjectFragm
         implements InitiatorProjectFragmentContract.View {
     public static final String TAG = TagProvider.getDebugTag(InitiatorProjectFragment.class);
 
+    private ListView projectLV;
+    private ProjectAdapter adapter;
+
+
 
     public InitiatorProjectFragment() {
         // Required empty public constructor
@@ -35,7 +41,25 @@ public class InitiatorProjectFragment extends BaseFragment<InitiatorProjectFragm
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_plan_desc, container, false);
+        View convertView = inflater.inflate(R.layout.fragment_initiator_project, container, false);
+        projectLV = convertView.findViewById(R.id.projectListView);
+        adapter = new ProjectAdapter(activityContext, ProjectListGenerator.generateProjects());
+        projectLV.setAdapter(adapter);
+
+        projectLV.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(activityContext, "position: "+ProjectListGenerator.generateProjects().get(position), Toast.LENGTH_SHORT).show();
+
+                /*startActivity(new Intent(MainActivity.this,DetailsActivity.class)
+                .putExtra("pos",position));*/
+            }
+        });
+        return convertView;
+    }
+
+    public void addProject(View view) {
+
     }
 
     @Override
--- app/src/main/java/com/cryptenet/thanatos/dtmweb/home/request_detail/RequestDetailFragment.java
+++ app/src/main/java/com/cryptenet/thanatos/dtmweb/home/request_detail/RequestDetailFragment.java
@@ -35,7 +35,7 @@ public class RequestDetailFragment extends BaseFragment<RequestDetailFragmentCon
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_plan_desc, container, false);
+        return inflater.inflate(R.layout.fragment_request_detail, container, false);
     }
 
     @Override
