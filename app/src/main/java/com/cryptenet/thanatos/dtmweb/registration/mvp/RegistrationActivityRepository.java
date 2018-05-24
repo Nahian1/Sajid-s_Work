f14bd1059b91d114eefd998a05d74c115a46de32
Alamgir Hossain <al.hossain94@gmail.com>
2018-05-24 16:50:14 +0600

    dev commit 24052018 1650

--- app/src/main/java/com/cryptenet/thanatos/dtmweb/registration/mvp/RegistrationActivityRepository.java
+++ app/src/main/java/com/cryptenet/thanatos/dtmweb/registration/mvp/RegistrationActivityRepository.java
@@ -28,9 +28,6 @@ import org.greenrobot.eventbus.EventBus;
 import java.util.ArrayList;
 import java.util.List;
 
-import okhttp3.MediaType;
-import okhttp3.MultipartBody;
-import okhttp3.RequestBody;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Callback;
