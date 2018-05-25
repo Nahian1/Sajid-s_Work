4456a4c4097419e12e47584171593e9a47e744f0
nabilnealsh <nabil.shawkat023@gmail.com>
2018-05-26 01:48:35 +0600

    25052018 1543

--- app/src/main/java/com/cryptenet/thanatos/dtmweb/home/plan_long_desc/PlanLongDescFragment.java
+++ app/src/main/java/com/cryptenet/thanatos/dtmweb/home/plan_long_desc/PlanLongDescFragment.java
@@ -1,11 +1,11 @@
 /*
  * Copyright (c) 2018.
- *  Development Courtesy: Cryptenet Ltd.
- *  Developer Credit: Alamgir Hossain, Nabil Shawkat
- *  This project is under MIT license
+ * Development Courtesy: Cryptenet Ltd.
+ * Developer Credit: Alamgir Hossain, Nabil Shawkat
+ * This project is under MIT license
  */
 
-package com.cryptenet.thanatos.dtmweb.home.plan_desc;
+package com.cryptenet.thanatos.dtmweb.home.plan_long_desc;
 
 
 import android.os.Bundle;
@@ -19,21 +19,21 @@ import android.widget.Toast;
 
 import com.cryptenet.thanatos.dtmweb.R;
 import com.cryptenet.thanatos.dtmweb.base.BaseFragment;
-import com.cryptenet.thanatos.dtmweb.mvp_contracts.PlanDescFragmentContract;
+import com.cryptenet.thanatos.dtmweb.mvp_contracts.PlanLongDescFragmentContract;
 import com.cryptenet.thanatos.dtmweb.pojo.Projects;
 import com.cryptenet.thanatos.dtmweb.utils.providers.TagProvider;
 
 
-public class PlanDescFragment extends BaseFragment<PlanDescFragmentContract.Presenter>
-        implements PlanDescFragmentContract.View {
-    public static final String TAG = TagProvider.getDebugTag(PlanDescFragment.class);
+public class PlanLongDescFragment extends BaseFragment<PlanLongDescFragmentContract.Presenter>
+        implements PlanLongDescFragmentContract.View {
+    public static final String TAG = TagProvider.getDebugTag(PlanLongDescFragment.class);
     private Projects projects;
-    private TextView titleTV, priceTV, shortDetailsTV, nameTV, typeTV, addressTV, detailsTV, bankNameTV,
-            personNameTV, accountNumberTV, payPriceTV;
-    private ImageView demoIV,profileIV,seeMoreIV;
+    private TextView titleTV, priceTV, shortDetailsTV, nameTV, typeTV, addressTV, detailsTV, longDescTV;
+    private ImageView demoIV,profileIV;
 
 
-    public PlanDescFragment() {
+
+    public PlanLongDescFragment() {
         // Required empty public constructor
     }
 
@@ -42,8 +42,10 @@ public class PlanDescFragment extends BaseFragment<PlanDescFragmentContract.Pres
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View convertView = inflater.inflate(R.layout.fragment_plan_desc, container, false);
+        View convertView = inflater.inflate(R.layout.fragment_plan_long_desc, container, false);
+
         projects = (Projects) getArguments().get("project");
+
         titleTV = convertView.findViewById(R.id.textViewTitle);
         priceTV = convertView.findViewById(R.id.textViewPrice);
         shortDetailsTV = convertView.findViewById(R.id.textViewDetails);
@@ -51,21 +53,12 @@ public class PlanDescFragment extends BaseFragment<PlanDescFragmentContract.Pres
         typeTV = convertView.findViewById(R.id.textViewType);
         addressTV = convertView.findViewById(R.id.address);
         detailsTV = convertView.findViewById(R.id.details);
-        bankNameTV = convertView.findViewById(R.id.editTextBankName);
-        personNameTV = convertView.findViewById(R.id.NameOfperson);
-        accountNumberTV = convertView.findViewById(R.id.accountNumber);
-        payPriceTV = convertView.findViewById(R.id.price);
+        longDescTV = convertView.findViewById(R.id.longDesc);
 
-        demoIV = convertView.findViewById(R.id.projectIV);
+        demoIV = convertView.findViewById(R.id.demoImg);
         profileIV = convertView.findViewById(R.id.profilepic);
-        seeMoreIV = convertView.findViewById(R.id.seeMoreIV);
-
 
         return convertView;
-    }
-
-    public void requestforDetails(){
-
     }
 
     @Override
---
+++ app/src/main/java/com/cryptenet/thanatos/dtmweb/mvp_contracts/PlanLongDescFragmentContract.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2018.
+ * Development Courtesy: Cryptenet Ltd.
+ * Developer Credit: Alamgir Hossain, Nabil Shawkat
+ * This project is under MIT license
+ */
+
+package com.cryptenet.thanatos.dtmweb.mvp_contracts;
+
+public interface PlanLongDescFragmentContract {
+    interface Presenter extends BaseFragContract.Presenter<PlanLongDescFragmentContract.View> {
+    }
+
+    interface View extends BaseFragContract.View {
+    }
+
+    interface Model extends BaseFragContract.Model {
+    }
+
+    interface Repository extends BaseFragContract.Repository {
+    }
+}
